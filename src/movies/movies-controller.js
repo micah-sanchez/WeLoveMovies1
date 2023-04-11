14d9155492866469387c9cc054f24d2f315963b4
micah-sanchez <mjsanch4@gmail.com>
2023-04-10 23:46:45 -0600

    added list function for all movies and param equals true

--- src/movies/movies-controller.js
+++ src/movies/movies-controller.js
@@ -2,9 +2,10 @@ const service = require("./movies-service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function movieExists(req, res, next) {
-  const { movieId } = req.params;
-
-  const movie = await service.read(movieId);
+  
+  //const { movieId } = req.params;
+  console.log(req.params.movieId)
+  const movie = await service.read(req.params.movieId);
   if (movie) {
     res.locals.movie = movie;
     return next();
@@ -12,26 +13,27 @@ async function movieExists(req, res, next) {
   return next({ status: 404, message: `Movie cannot be found.` });
 }
 
-async function read(req, res, next) {
-  const { movie } = res.locals;
-  res.json({ data: movie });
-}
-
-async function list(req, res, next) {
-  const data = await service.list();
+function read(req, res, next) {
+  const { movie: data } = res.locals;
   res.json({ data });
 }
 
-async function validateIsTrue(req, res, next) {
-  const { isTrue } = req.query;
-
-  if (isTrue === true) {
-    return next();
+async function list(req, res, next) {
+  const isTrue = req.query
+  console.log("hello!")
+  //console.log(JSON.stringify(isTrue))
+  if (isTrue.is_showing === "true") {
+    console.log("hits true if")
+    const data = await service.listShowingMovies()
+    res.json({ data })
+  } else {
+    console.log("hits else")
+    const data = await service.listAll();
+    res.json({ data });
   }
-  return next({ status: 404, message: `Something went wrong.` });
 }
 
 module.exports = {
-  read: [asyncErrorBoundary(movieExists), read],
+  read: [asyncErrorBoundary(movieExists), asyncErrorBoundary(read)],
   list: asyncErrorBoundary(list),
 };
