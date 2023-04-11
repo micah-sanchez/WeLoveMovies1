93a707f7e3c8a42b758f2c762ee179994a3c4ead
micah-sanchez <mjsanch4@gmail.com>
2023-04-10 23:45:52 -0600

    added error handlers

--- src/app.js
+++ src/app.js
@@ -11,4 +11,13 @@ app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
 app.use("/theaters", theatersRouter);
 
+app.use((req, res, next) => {
+    next({ status: 404, message: `Not found: ${req.originalUrl}` })
+})
+
+app.use((error, req, res, next) =>  {
+    const { status = 500, message = "Something went wrong"} = error;
+    res.status(status).json( {error: message} )
+});
+
 module.exports = app;
