20eb42f6ac1adb3c4ab5a630adee28c50e980f7d
micah-sanchez <mjsanch4@gmail.com>
2023-04-10 23:48:42 -0600

    added read query

--- src/movies/movies-service.js
+++ src/movies/movies-service.js
@@ -1,10 +1,25 @@
 const knex = require("../db/connection");
 
-function list() {
+function listAll() {
     return knex("movies")
       .select("*")
   }
 
+function listShowingMovies() {
+  return knex("movies as m")
+    .join("movies_theatres as mt")
+    .select("m.*")
+    .where("mt.is_showing", "=", "true")
+}
+
+function read(movieId) {
+  return knex("movies")
+  .select("*")
+  .where({ movie_id: movieId })
+}
+
 module.exports = {
-    list,
+    listAll,
+    listShowingMovies,
+    read
 }
