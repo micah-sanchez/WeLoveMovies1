9b2753fc5095310b09e82b4da40747cb739970aa
micah-sanchez <mjsanch4@gmail.com>
2023-04-11 23:31:42 -0600

    debugged list function

--- src/theaters/theaters-controller.js
+++ src/theaters/theaters-controller.js
@@ -1,4 +1,10 @@
 const service = require("./theaters-service");
 
+async function list(req, res, next) {
+    const data  = await service.listTheaters();
+    res.json({ data })
+}
 
-module.exports = {}
+module.exports = {
+    list,
+}
