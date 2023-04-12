3ca76fa306dc98e80fdfb95379d461606a4e03ed
micah-sanchez <mjsanch4@gmail.com>
2023-04-11 23:56:45 -0600

    updated router to put method

--- src/reviews/reviews-router.js
+++ src/reviews/reviews-router.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
+const methodNotAllowed = require("../errors/methodNotAllowed");
 const controller = require("./reviews-controller");
 
-router.route("/")
+router.route("/:reviewId")
+    .put(controller.update)
+    .all(methodNotAllowed)
 
 module.exports = router;
