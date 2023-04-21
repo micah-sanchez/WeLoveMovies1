0fa381f277c38b1e06208639459e7cdabfa22f38
micah-sanchez <mjsanch4@gmail.com>
2023-04-20 21:47:05 -0600

    reworked queries to map critics info

--- src/reviews/reviews-service.js
+++ src/reviews/reviews-service.js
@@ -2,19 +2,38 @@ const knex = require("../db/connection");
 const mapProperties = require("../utils/map-properties");
 
 function read(reviewId) {
-    return knex("reviews")
-        .select("*")
-        .limit(1)
+  return knex("reviews").select("*").where({ review_id: reviewId });
 }
 
-function update(updatedReview) {
-    return knex("reviews")
-      .select("*")
-      .where({ review_id: updatedReview.review_id })
-      .update(updatedReview, "*");
-  }
+const addCritic = mapProperties({
+    preferred_name: "critic.preferred_name",
+    surname: "critic.surname",
+    organization_name: "critic.organization_name",
+  });
+
+function listReviews(id) {
+  return knex("reviews as r")
+    .join("critics as c", "c.critic_id", "r.critic_id")
+    .select("r.*", "c.*")
+    .where({ "r.review_id": id })
+    .then((data) => data.map((r) => addCritic(r)));
+}
+
+function update(updatedReview, review_id) {
+  return knex("reviews")
+    .select("*")
+    .where({review_id})
+    .update(updatedReview)
+    .then(() => listReviews(review_id))
+    .then(updatedRecords => updatedRecords[0])
+};
+
+function destroy(reviewId) {
+  return knex("reviews").select("*").where({ review_id: reviewId }).del();
+}
 
 module.exports = {
-    read,
-    update
-}
+  read,
+  update,
+  delete: destroy,
+};
